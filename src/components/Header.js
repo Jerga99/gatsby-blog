9d1e43af0f9f8c85ea74d20da7f6a667b5580775
Filip Jerga <jerga99@gmail.com>
2021-04-06 09:10:43 +0100

    Gatsby Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Header() {
   return (
     <div>
       <h1>I am Header</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/blogs">Blogs</Link>
     </div>
   )
 }
---
+++ src/pages/blogs.js
@@ -0,0 +1,9 @@
+
+import React from "react"
+
+export default function Blogs() {
+
+  return (
+    <h1>I am blogs page!</h1>
+  )
+}
