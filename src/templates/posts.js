de9edb383124d2f9d7ea8b1252053e1a1af3800e
Filip Jerga <jerga99@gmail.com>
2021-04-07 09:36:19 +0100

    Iterate posts

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -2,12 +2,20 @@
 
 import React from "react"
 import Layout from "../components/Layout"
+import { Link } from "gatsby"
 
 export default function Posts({pageContext: {posts}}) {
   return (
     <Layout>
       <h1>I am posts page</h1>
-      <p>{JSON.stringify(posts)}</p>
+      <ul>
+        { posts.map(post =>
+          <li key={post.id}>
+            <Link to="/">{post.title}</Link>
+          </li>
+          )
+        }
+      </ul>
     </Layout>
   )
 }
