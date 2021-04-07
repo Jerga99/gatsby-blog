b1d73ee1c4147586675cd3a2f7b8b14bcd60665c
Filip Jerga <jerga99@gmail.com>
2021-04-07 08:25:28 +0100

    about styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 
 import React from "react"
 import { Link } from "gatsby"
+import "./Header.scss";
 
 export default function Header() {
   return (
     <div>
-      <h1>I am Header</h1>
+      <h1 className="header-title">I am Header</h1>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/blogs">Blogs</Link>
