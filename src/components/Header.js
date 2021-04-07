ec8164745986de5d90540efd05a8bae23d529115
Filip Jerga <jerga99@gmail.com>
2021-04-07 08:43:14 +0100

    Module styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 
 import React from "react"
 import { Link } from "gatsby"
-import "./Header.scss";
+
+import * as headerStyles from "./Header.module.scss"
 
 export default function Header() {
   return (
     <div>
-      <h1 className="header-title">I am Header</h1>
+      <h1 className={headerStyles.headerTitle}>I am Header</h1>
+      <h2 className={headerStyles.headerSubtitle}>I am another header</h2>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/blogs">Blogs</Link>
