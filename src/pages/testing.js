f8d7873880882fc19d6c36d3102bf542c48c3a80
Filip Jerga <jerga99@gmail.com>
2021-04-09 09:39:01 +0100

    Get node to page

--- src/pages/testing.js
+++ src/pages/testing.js
@@ -1,32 +1,35 @@
 
 
-// import React from "react"
-// import Layout from "../components/Layout"
-// import { Link, graphql } from "gatsby"
+import React from "react"
+import Layout from "../components/Layout"
+import { Link, graphql } from "gatsby"
 
-// export default function TestingPage({data}) {
-//   const posts = data.allPost
-//   return (
-//     <Layout>
-//       <h1>I am posts page</h1>
-//       <ul>
-//         { posts.map(post =>
-//           <li key={post.id}>
-//             <Link to={`/posts/${post.id}`}>{post.title}</Link>
-//           </li>
-//           )
-//         }
-//       </ul>
-//     </Layout>
-//   )
-// }
+export default function TestingPage({data}) {
+  const posts = data.allPost.nodes
+  debugger
+  return (
+    <Layout>
+      <h1>I am posts page</h1>
+      <ul>
+        { posts.map(post =>
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+          )
+        }
+      </ul>
+    </Layout>
+  )
+}
 
-// export const query = graphql`
-//  query {
-//    allPost {
-//      id
-//      title
-//      body
-//    }
-//  }
-// `
+export const query = graphql`
+ query {
+   allPost {
+     nodes {
+      id
+      title
+      body
+     }
+   }
+ }
+`
