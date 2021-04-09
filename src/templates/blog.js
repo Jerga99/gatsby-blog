8add6384a3711aff5264410347e42d3f7df0709b
Filip Jerga <jerga99@gmail.com>
2021-04-09 12:38:09 +0100

    Single query

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,13 +1,22 @@
 
 import React from "react";
 import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
-export default function Blog({pageContext: {slug}}) {
+export default function Blog({data}) {
 
   return (
     <Layout>
       <h1>I am blog detail page</h1>
-      <p>{slug}</p>
+      <p>{data.markdownRemark.html}</p>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($slug: String) {
+    markdownRemark(fields: {slug: {eq: $slug}}) {
+      html
+    }
+  }
+`
