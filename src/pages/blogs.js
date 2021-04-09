968b9e727407566729ca9c0227c00528df6460bc
Filip Jerga <jerga99@gmail.com>
2021-04-09 10:15:06 +0100

    Source data to blogs page

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,12 +1,39 @@
 
 import React from "react"
 import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
-export default function Blogs() {
+export default function Blogs({data}) {
 
   return (
     <Layout>
-      <h1>I am blogs page!</h1>
+      <ul>
+        { data.allFile.nodes.map(node =>
+          <li key={node.relativePath}>
+            <p>{node.relativePath}</p>
+            <p>{node.extension}</p>
+            <p>{node.birthTime}</p>
+            <p>{node.prettySize}</p>
+          </li>
+          )
+        }
+      </ul>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        extension
+        relativePath
+        birthTime(fromNow: true)
+        prettySize
+      }
+    }
+  }
+`
+
+// allFile
+// relativePath, prettySize, extension, birthtime
