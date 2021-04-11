09ef43ec298acd9133d98a339c979c205a22c9ce
Filip Jerga <jerga99@gmail.com>
2021-04-11 17:01:01 +0100

    Removes obsolete files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,5 @@
 
-const axios = require("axios")
-const { createFilePath } = require("gatsby-source-filesystem")
-
 exports.createPages = async ({graphql, actions: {createPage}}) => {
-  const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
-  const posts = res.data
-
   const result = await graphql(`
     query {
       allMarkdownRemark {
@@ -21,70 +15,11 @@ exports.createPages = async ({graphql, actions: {createPage}}) => {
 
   nodes.forEach(node => {
     createPage({
-      path: node.frontmatter.slug,
+      path: `/blogs/${node.frontmatter.slug}`,
       component: require.resolve("./src/templates/blog.js"),
       context: {
         slug: node.frontmatter.slug
       }
     })
   })
-
-  posts.forEach(post => {
-    createPage({
-      path: `/posts/${post.id}`,
-      component: require.resolve("./src/templates/post.js"),
-      context: { post }
-    })
-  })
-
-  createPage({
-    path: "/posts",
-    component: require.resolve("./src/templates/posts.js"),
-    context: { posts }
-  })
 }
-
-exports.sourceNodes = async ({actions, createNodeId, createContentDigest}) => {
-  const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
-  const posts = res.data
-
-  posts.forEach(post => {
-    const node = {
-      title: post.title,
-      body: post.body,
-      // The node ID must be globally unique
-      id: createNodeId(`Post-${post.id}`),
-      // id: `Post-${post.id}`,
-      // ID to the parent Node
-      parent: null,
-      // ID to the children Nodes
-      children: [],
-      // internal fields are not usualy interesting for consumers
-      // but are very important for Gatsby Core
-      internal: {
-        // globbaly unique node type
-        type: "Post",
-        // "Hash" or short digital summary of this node
-        contentDigest: createContentDigest(post),
-        // content exposing raw content of this node
-        content: JSON.stringify(post)
-      }
-    }
-
-    actions.createNode(node)
-  })
-}
-
-// exports.onCreateNode = ({node, getNode, actions}) => {
-
-//   if (node.internal.type === "MarkdownRemark") {
-//     const slug = createFilePath({node, getNode, basePath: "blogs"})
-
-//     actions.createNodeField({
-//       node,
-//       name: "slug",
-//       value: slug
-//     })
-//   }
-// }
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,41 @@
 
 import React from "react"
 import Layout from "../components/Layout"
+import { graphql, Link } from "gatsby"
 
-export default function Index() {
+export default function IndexPage({data}) {
 
+  const { totalCount, nodes } = data.allMarkdownRemark
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <h4>{totalCount} Posts</h4>
+      {nodes.map(({id, frontmatter, excerpt}) =>
+        <div key={id}>
+          <h3>
+            {frontmatter.title}
+            <span> - {frontmatter.date}</span>
+          </h3>
+          <p>{excerpt}</p>
+          <Link to={`/blogs/${frontmatter.slug}`}>Read More</Link>
+        </div>
+        )}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      nodes {
+        id
+        frontmatter {
+          title
+          date(formatString: "DD MMMM, YYYY")
+          slug
+        }
+        excerpt
+      }
+    }
+  }
+`
