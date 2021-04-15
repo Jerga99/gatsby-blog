8fe8e6456295a08dd9300500b4b5aea5ae86731f
Filip Jerga <jerga99@gmail.com>
2021-04-15 08:52:13 +0100

    Open Graph

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -3,7 +3,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-export default function Seo({title, description}) {
+export default function Seo({title, description, meta = []}) {
 
   const { site } = useStaticQuery(
     graphql`
@@ -21,6 +21,7 @@ export default function Seo({title, description}) {
   const defaultTitle = title ? `${title} | ${site.siteMetadata?.title}` :  site.siteMetadata?.title
   const defaultDescription = description || site.siteMetadata?.description
 
+  // TODO: og:url
   return (
     <Helmet
       htmlAttributes={{
@@ -31,8 +32,44 @@ export default function Seo({title, description}) {
         {
           name: "description",
           content: defaultDescription
-        }
-      ]}
+        },
+        {
+          name: "og:title",
+          content: defaultTitle
+        },
+        {
+          name: "og:description",
+          content: defaultDescription
+        },
+        {
+          name: "og:type",
+          content: "website"
+        },
+        {
+          name: "og:image",
+          content: "https://files.cdn.thinkific.com/courses/course_card_image_000/896/4641598445801.medium.png"
+        },
+        {
+          name: "twitter:card",
+          content: "summary"
+        },
+        {
+          name: "twitter:creator",
+          content: "Code Space"
+        },
+        {
+          name: "twitter:title",
+          content: defaultTitle
+        },
+        {
+          name: "twitter:description",
+          content: defaultDescription
+        },
+        {
+          name: "twitter:image",
+          content: "https://files.cdn.thinkific.com/courses/course_card_image_000/896/4641598445801.medium.png"
+        },
+      ].concat(meta)}
     />
   )
 }
