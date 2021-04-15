7760c1a5aa037c5616f84807d7bc8fb0037e5b39
Filip Jerga <jerga99@gmail.com>
2021-04-15 11:50:35 +0100

    Rss generator

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,25 @@
+
+const rss = require("./utils/rss-options")
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: "Place to learn coding",
     description: "The best resource to learn coding online",
+    siteUrl: process.env.BASE_URL,
     body: {
       content: "Just some SEO content"
     }
   },
   plugins: [
     "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-feed",
+      options: rss.options
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
